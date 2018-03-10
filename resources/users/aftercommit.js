e10c5e066afc89f1fb425dc89ad59ab87f1e9a36
Singh <Rajeev.Singh@parexel.com>
2018-03-10 17:15:06 +0000

    first version

---
+++ resources/users/aftercommit.js
@@ -0,0 +1,14 @@
+var parUserId = ctx.req.body.parentUserId;
+
+console.log('Provided Parent User Id is ' + parUserId);
+
+if(parUserId == 'undefined')
+    parUserId = id;
+
+dpd.profile.post({"name": ctx.req.body.username,"dob":ctx.req.body.dob,"mobileNumber":ctx.req.body.mobileNumber,
+                    "paymentLink":ctx.req.body.paymentLink,"parentUserId":parUserId}, function(result, err) {
+  if(err) return console.log(err);
+  console.log(result, result.id);
+});
+
+
